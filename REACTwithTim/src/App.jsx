53916a5342e9000dc570f1cfe783e58572851717
unknown <ibfatade18@gmail.com>
2025-08-30 19:40:54 +0100

    importing stylesheets

--- REACTwithTim/src/App.jsx
+++ REACTwithTim/src/App.jsx
@@ -1,17 +1,21 @@
 import Home from './pages/Home'
-import './App.css'
+import './css/App.css'
 import {Routes, Route} from "react-router-dom"
 import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <main className='main-content'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/favorites' element={<Favorites />} />
-        </Routes>
-    </main>
+    <>
+      <NavBar />
+      <main className='main-content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+      </main>
+    </>
   )
 }
 
