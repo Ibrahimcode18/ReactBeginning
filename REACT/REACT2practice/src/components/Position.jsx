30c001d29cece0eea6d57f9552cee98b8d6b7fb8
Ibrahim Fatade <ibfatade18@gmail.com>
2025-09-21 17:10:19 +0100

    displaying the letters on the selected position using useState

--- REACT/REACT2practice/src/components/Position.jsx
+++ REACT/REACT2practice/src/components/Position.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 import './Position.css'
 
 export default function Position(){
+    const [position,setPosition] = useState()
     return(
         <>
-            <button className="box">
-
+            <button className="box" onClick={() => setPosition("X")}>
+                {position}
             </button>
         </>
     )
