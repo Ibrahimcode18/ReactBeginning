0fae4d49710f2f15343ea753e46ace1fdba7e8ee
Ibrahim Fatade <ibfatade18@gmail.com>
2025-09-23 08:23:11 +0100

    restructuring the internal and external gameboards and enabling switch between players

--- REACT/REACT2practice/src/App.jsx
+++ REACT/REACT2practice/src/App.jsx
@@ -4,8 +4,27 @@ import Position from './components/Position'
 import Player from './components/Player'
 
 function App() {
+  const initialBoard = [
+    null,null,null,
+    null,null,null,
+    null,null,null
+  ]
   const [count, setCount] = useState(0)
+  const [activePlayer, setActivePlayer] = useState("X")
+  
+  const [board, setBoard] = useState(initialBoard)
 
+  function handleChange(index){
+    if (board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = activePlayer;
+    setBoard(newBoard);
+
+    // Switch player
+    setActivePlayer((prev) => (prev === "X" ? "O" : "X"));
+
+  }
   return (
     <>
       <img src='/game-logo.png' className='logo' />
@@ -19,19 +38,19 @@ function App() {
 
         <div className='positions'>
           <div className='row1'>
-            <Position />
-            <Position />
-            <Position />
+            <Position value={board[0]} onClick={() => handleChange(0)}/>
+            <Position value={board[1]} onClick={() => handleChange(1)}/>
+            <Position value={board[2]} onClick={() => handleChange(2)}/>
           </div>
           <div className='row2'>
-            <Position />
-            <Position />
-            <Position />
+            <Position value={board[3]} onClick={() => handleChange(3)}/>
+            <Position value={board[4]} onClick={() => handleChange(4)}/>
+            <Position value={board[5]} onClick={() => handleChange(5)}/>
           </div>
           <div className='row3'>
-            <Position />
-            <Position />
-            <Position />
+            <Position value={board[6]} onClick={() => handleChange(6)}/>
+            <Position value={board[7]} onClick={() => handleChange(7)}/>
+            <Position value={board[8]} onClick={() => handleChange(8)}/>
           </div>
         </div>
         
--- REACT/REACT2practice/src/components/Position.jsx
+++ REACT/REACT2practice/src/components/Position.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './Position.css'
 
-export default function Position(){
-    const [position,setPosition] = useState()
+export default function Position({onClick,value}){
+
     return(
         <>
-            <button className="box" onClick={() => setPosition("X")}>
-                {position}
+            <button className="box" onClick={onClick}>
+                {value}
             </button>
         </>
     )
